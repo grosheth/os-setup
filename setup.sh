7bfa1d2e39f5771ede356ad02b676c5b9858683d
salledelavage <alextrington@gmail.com>
2022-11-17 07:26:34 -0500

    commit automatique

--- setup.sh
+++ setup.sh
@@ -12,21 +12,53 @@ WHITE='\033[1;37m'        # White
 
 # vars
 PROGRAMS=$(cat programs.txt)
+OS=$(uname -r)
 
-# Update the System
-sudo pacman -Syu --noconfirm
 
-# Install all programs listed in the txt file
-for i in $PROGRAMS
-do
-    echo -e "${YELLOW} --- installation de $i ---"
-    sudo pacman -Syu $i --noconfirm;
+package_install () {
+
+    for i in $PROGRAMS
+    do
+        echo -e "${YELLOW} --- installation de $i ---"
+        sudo $1 $2 $i $3;
+        echo -e "${GREEN} --- Done ---"
+    done
+
+}
+
+arch_install () {
+
+    # update system
+    sudo pacman -Syu --noconfirm
+
+    # Install all programs listed in the txt file
+    package_install pacman -Syu --noconfirm
+
+    echo -e "${YELLOW} --- Mise a jour du .zshrc ---"
+
+    # Replace .zshrc
+    cat alias/.zshrc > ~/.zshrc
+    cat ~/.zshrc
     echo -e "${GREEN} --- Done ---"
-done
 
-echo -e "${YELLOW} --- Mise a jour du .zshrc ---"
+}
 
-# Replace .zshrc
-cat alias/.zshrc > ~/.zshrc
-cat ~/.zshrc
-echo -e "${GREEN} --- Done ---"
+
+debian_install () {
+
+    # update system
+    sudo apt update && sudo apt upgrade -y
+
+    # Install all programs listed in the txt file
+    package_install apt install -y
+
+    echo -e "${YELLOW} --- Mise a jour du .zshrc ---"
+
+    # Replace .zshrc
+    cat alias/.zshrc > ~/.zshrc
+    cat ~/.zshrc
+    echo -e "${GREEN} --- Done ---"
+
+}
+
+arch_install
