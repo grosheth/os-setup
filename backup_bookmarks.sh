1162f5cc3c86d54895f673555905c480f4163f92
salledelavage <alextrington@gmail.com>
2022-11-26 12:52:58 -0500

    mise a jour des bookmarks via le crontab

--- backup_bookmarks.sh
+++ backup_bookmarks.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
+#Setup a crontab to make sure Bookmarks are always saved
 cp ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks files/
+git add .
+git commit -m "mise a jour des bookmarks via le crontab"
+git push
